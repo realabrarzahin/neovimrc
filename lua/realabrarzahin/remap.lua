c2db8dd1261d7d936672a9da02ebbacce6aea08f
Md Abrar Zahin Antor <realabrarzahin@gmail.com>
2025-12-17 20:21:18 +0600

    fix/cursor+duplicate line+move behavior

--- lua/realabrarzahin/remap.lua
+++ lua/realabrarzahin/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
@@ -6,4 +7,4 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv")
 
-vim.keymap.set({ "v", "i", "n" }, "<leader>d", "mmyyp`mj")
+vim.keymap.set({ "v", "i", "n" }, "D", "mmyyp`mj")
